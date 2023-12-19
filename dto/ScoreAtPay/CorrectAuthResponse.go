8816debfe514b2eee946350a6cb3c276df2c6505
Evan Kerik <ekerik220@gmail.com>
2023-12-19 15:47:11 +0900

    add correct auth req/res

---
+++ dto/ScoreAtPay/CorrectAuthResponse.go
@@ -0,0 +1,16 @@
+package ScoreAtPay
+
+type CorrectAuthResponse struct {
+	ServiceType         string  `json:"serviceType,omitempty"`
+	Mstatus             string  `json:"mstatus,omitempty"`
+	VResultCode         string  `json:"vResultCode,omitempty"`
+	MerrMsg             string  `json:"merrMsg,omitempty"`
+	MarchTxn            string  `json:"marchTxn,omitempty"`
+	OrderId             string  `json:"orderId,omitempty"`
+	CustTxn             string  `json:"custTxn,omitempty"`
+	TxnVersion          string  `json:"txnVersion,omitempty"`
+	ShopTransactionId   string  `json:"shopTransactionId,omitempty"`
+	NissenTransactionId string  `json:"nissenTransactionId,omitempty"`
+	AuthorResult        string  `json:"authorResult,omitempty"`
+	Errors              []Error `json:"errors,omitempty"`
+}
